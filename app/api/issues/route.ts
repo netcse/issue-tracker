c8d11453c5d675c9a08a205414f7cdf26c28c3c5
Amran <imran.cse.kuet@gmail.com>
2025-03-28 21:19:43 +0600

    Build an API for updating issues

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,23 +1,18 @@
-import {NextRequest, NextResponse} from "next/server";
-import {z} from "zod";
-import {prisma} from "@/prisma/client";
-
-const createIssueSchema = z.object({
-    title: z.string().min(1).max(255),
-    description: z.string().min(1),
-});
+import { issueSchema } from "@/app/validationSchemas";
+import { prisma } from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
-    const body = await req.json();
-    const validation = createIssueSchema.parse(body);
-    if (!validation)
-        return NextResponse.json({error: "Invalid request"}, {status: 400});
+  const body = await req.json();
+  const validation = issueSchema.parse(body);
+  if (!validation)
+    return NextResponse.json({ error: "Invalid request" }, { status: 400 });
 
-    const newIssue = await prisma.issue.create({
-        data: {
-            title: validation.title,
-            description: validation.description,
-        },
-    });
-    return NextResponse.json(newIssue, {status: 201});
+  const newIssue = await prisma.issue.create({
+    data: {
+      title: validation.title,
+      description: validation.description,
+    },
+  });
+  return NextResponse.json(newIssue, { status: 201 });
 }
---
+++ app/validationSchemas.ts
@@ -0,0 +1,7 @@
+import { z } from "zod";
+
+
+export const issueSchema = z.object({
+    title: z.string().min(1).max(255),
+    description: z.string().min(1),
+});
